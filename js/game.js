2bc813a6bbf4573442252a2eff8b6bdd93a19deb
Allan <workofcode@gmail.com>
2021-05-09 22:14:39 +0100

    Setup
    - Initial setup of the project, using the MelonJS bootstrap

---
+++ js/game.js
@@ -0,0 +1,35 @@
+
+/* Game namespace */
+var game = {
+
+    // an object where to store game information
+    data : {
+        // score
+        score : 0
+    },
+
+
+    // Run on page load.
+    "onload" : function () {
+        // Initialize the video.
+        if (!me.video.init(960, 640, {parent : "screen", scale : "auto", scaleMethod : "flex"})) {
+            alert("Your browser does not support HTML5 canvas.");
+            return;
+        }
+
+        // Initialize the audio.
+        me.audio.init("mp3,ogg");
+
+        // set and load all resources.
+        // (this will also automatically switch to the loading screen)
+        me.loader.preload(game.resources, this.loaded.bind(this));
+    },
+
+    // Run on game resources loaded.
+    "loaded" : function () {
+        me.state.set(me.state.PLAY, new game.PlayScreen());
+
+        // Start the game.
+        me.state.change(me.state.PLAY);
+    }
+};
